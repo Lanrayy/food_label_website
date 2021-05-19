1d180bbd94cb859714dae62eacd8d932d9093573
Olanrewaju Sodeinde <bn16os@leeds.ac.uk>
2021-05-19 01:09:09 +0100

    Print styles completed

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,70 @@
 $(function(){
 
     $("#submit-button").on("click",function(){
-        //$(".output-container").slideToggle(2000);
-        //$(".output-container").toggle();
+
+        // COLLECT THE INFOMRATION NEEDED
         var energyKJ = document.querySelector('#energyKJ').value;
         var energyKcal = document.querySelector('#energyKcal').value;
         var protein = document.querySelector('#protein').value;
         var fat = document.querySelector('#fat').value;
         var saturated_fat = document.querySelector('#saturated-fat').value;
+        var poly_unsaturated_fat = document.querySelector('#poly-unsaturated-fat').value;
+        var mono_unsaturated_fat = document.querySelector('#mono-unsaturated-fat').value;
         var carbohydrates = document.querySelector('#carbohydrates').value;
         var sugars = document.querySelector('#sugars').value;
+        var dietary_fibre = document.querySelector('#dietary-fibre').value;
+        var sodium = document.querySelector('#sodium').value;
+        var moisture = document.querySelector('#moisture').value;
+        var ash = document.querySelector('#ash').value;
         var salt = document.querySelector('#salt').value;
+        var date = document.querySelector('#date').value;
+
+        console.log(date);
+
+
+        // DISPLAY the back of pack information
+        $("#bop-energyKJ").html(energyKJ+"KJ / 100g");
+        $("#bop-energyKcal").html(energyKcal+"Kcal / 100g");
+
+        //fat   
+        $("#bop-fat").html(fat+"g");
+
+        // Back of pack saturates
+        $("#bop-saturates").html(saturated_fat+"g");
+
+        //Back of pack carbohydrates
+        $("#bop-carbohydrates").html(carbohydrates+"g");
+
+        //Back of pack protein 
+        $("#bop-protein").html(protein+"g");
+
+        //back of pack Salt
+        $("#bop-salt").html(salt+"g");
+
+
+
+
+        // DISPLAY THE VALUES THAT IS DISPLAYED WHEN PRINTING
+        $("#print-energy").html(energyKcal+"/"+energyKJ);
+        $("#print-protein").html(protein);
+        $("#print-fat").html(fat);
+        $("#print-saturated-fat").html(saturated_fat);
+        $("#print-carbohydrates").html(carbohydrates);
+        $("#print-sugars").html(sugars);
+        $("#print-salt").html(salt);
+        $("#print-poly-unsaturated-fat").html(poly_unsaturated_fat);
+        $("#print-mono-unsaturated-fat").html(mono_unsaturated_fat);
+        $("#print-dietary-fibre").html(dietary_fibre);
+        $("#print-sodium").html(sodium);
+        $("#print-moisture").html(moisture);
+        $("#print-ash").html(ash);
+        $("#date-of-report").html("Date of report: " + date);
+
+
+        // CALCULATE FRONT OF PACK INFORMATION
 
         //If the value is left empty then make it zero
-        if(energyKJ == "")
+        if(energyKJ == "" || energyKJ == "<0.1")
         {
             energyKJ = 0;
         }
@@ -23,39 +74,68 @@ $(function(){
             energyKcal = 0;
         }
         
-        if(fat == "")
+        if(fat == "" || fat == "<0.1")
         {
             fat = 0;
         }
 
-        if(saturated_fat == "")
+        if(saturated_fat == "" || saturated_fat == "<0.1")
         {
             saturated_fat = 0
         } 
 
-        if(sugars == "")
+        if(mono_unsaturated_fat == "" || mono_unsaturated_fat == "<0.1")
+        {
+            mono_unsaturated_fat = 0
+        } 
+
+        if(poly_unsaturated_fat == "" || poly_unsaturated_fat == "<0.1")
+        {
+            poly_unsaturated_fat = 0
+        } 
+
+        if(dietary_fibre == "" || dietary_fibre == "<0.1")
+        {
+            dietary_fibre = 0
+        } 
+
+        if(sodium == "" || sodium == "<0.1")
+        {
+            sodium = 0
+        } 
+
+        if(moisture == "" || moisture == "<0.1")
+        {
+            moisture = 0
+        } 
+
+        if(ash == "" || ash == "<0.1")
+        {
+            ash = 0
+        } 
+
+        if(sugars == "" || sugars == "<0.1")
         {
             sugars = 0;
         }
         
-        if(salt == "")
+        if(salt == "" || salt == "<0.1")
         {
             salt = 0;
         }
 
-        if(protein == "")
+        if(protein == "" || protein == "<0.1")
         {
             protein = 0;
         }
 
-        if(carbohydrates == "")
+        if(carbohydrates == "" || carbohydrates == "<0.1")
         {
             carbohydrates = 0;
         }
 
         
         //Calculate FOP percentages
-        
         var energy_fop_percentage = ((energyKJ/8400)* 100).toFixed(0);
         var fop_fat_percentage = ((fat/70)* 100).toFixed(0);
         var fop_saturated_fat_percentage = ((saturated_fat/20)* 100).toFixed(0);
@@ -63,7 +143,7 @@ $(function(){
         var fop_salt_percentage = ((salt/6)* 100).toFixed(0);
 
 
-        //Calculate the new values
+        //Calculate & display the new fop values
         $("#fop-KJ").html(energyKJ+'KJ');
         $("#fop-Kcal").html(energyKcal+'Kcal');
         $("#fop-energy-percentage").html( energy_fop_percentage+'%');
@@ -83,7 +163,7 @@ $(function(){
         $("#typical-values").html(energyKJ+ "KJ / "+energyKcal + "Kcal");
 
 
-        //Calculate the colour for the front of pack information
+        //Calculate the colours for the front of pack information
         //Fat
         if(fat > 17.5)
         {
@@ -134,25 +214,20 @@ $(function(){
 
 
 
-        // Calculate the back of pack information
-        //Energy
-        $("#bop-energyKJ").html(energyKJ+"KJ / 100g");
-        $("#bop-energyKcal").html(energyKcal+"Kcal / 100g");
 
-        //fat   
-        $("#bop-fat").html(fat+"g");
 
-        // Back of pack saturates
-        $("#bop-saturates").html(saturated_fat+"g");
 
-        //Back of pack carbohydrates
-        $("#bop-carbohydrates").html(carbohydrates+"g");
 
-        //Back of pack protein 
-        $("#bop-protein").html(protein+"g");
 
-        //back of pack Salt
-        $("#bop-salt").html(salt+"g");
+
+
+        
+
+
+
+
+
+
 
         //SHow the results
         $(".front-of-pack-container").fadeIn(1000);
